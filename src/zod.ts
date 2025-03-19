4e9fab8fa6b367cb5c6862ae4ed3ca9fd35f38ac
Luan <luan.santos@rspneus.com.br>
2025-03-19 14:35:57 -0300

    Validações

--- src/zod.ts
+++ src/zod.ts
@@ -1,11 +1,15 @@
 import { z } from "zod";
 
 const pattern = z.object({
-  name: z.literal('Luan'),
+  age: z
+    .number({
+      required_error: "Idade é obrigatória!!",
+      invalid_type_error: "Formatos de dados incorretos",
+    })
+    .gte(18, "Apenas para maiores de idade!!"),
 });
 
 const result = pattern.parse({
-  name: 'Luan'
-})
-
+  age: 19,
+});
 console.log(result);
